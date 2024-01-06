7635128be1f4c4adeb60c2b4da411cc77fa46245
Danielle Foss <danifoss2011@gmail.com>
2024-01-06 15:19:44 -0700

    add base code

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+//import React from "react";
+
+const Footer = () => {
+  console.log("hello from footer");
+
+  return console.log("hello from footer2");
+};
+
+export default Footer;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,9 @@
+//import React from "react";
+
+const Profile = () => {
+  console.log("hello from profile");
+
+  return console.log("hello from profile2");
+};
+
+export default Profile;
