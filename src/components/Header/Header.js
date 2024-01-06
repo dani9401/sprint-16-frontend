156457c622a1420317389142a67d9d3b0136df38
Danielle Foss <danifoss2011@gmail.com>
2024-01-06 15:17:28 -0700

    add basic styles and layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,35 @@
-const Header = () => {
-  console.log("hello from Header");
+import "../Header/Header.css";
+import Logo from "../../images/cbm_new-logo_png.jpg";
 
-  return console.log("hello from Header2");
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__menu-left">
+        <div className="header__logo">
+          <img src={Logo} alt="CBM Logo" className="header__logo-image"></img>
+        </div>
+        <h2 className="header__rescue-name">Colorado Bullies & More Rescue</h2>
+      </div>
+      <div className="header__menu-right">
+        <div className="header__menu-buttons">
+          <button
+            className="header__button"
+            //onClick={onRegisterModal}
+            type="text"
+          >
+            Sign Up
+          </button>
+          <button
+            className="header__button"
+            //onClick={onLoginModal}
+            type="text"
+          >
+            Log In
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
