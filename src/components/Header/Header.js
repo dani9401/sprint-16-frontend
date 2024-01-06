43839c0d730f1e9b4d064250634930fbea777184
Danielle Foss <danifoss2011@gmail.com>
2024-01-06 13:51:20 -0700

    add base code

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+  console.log("hello from Header");
+
+  return console.log("hello from Header2");
+};
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,7 @@
+const Main = () => {
+  console.log("hello from Main");
+
+  return console.log("hello from Main2");
+};
+
+export default Main;
