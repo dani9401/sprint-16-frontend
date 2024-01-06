0ec6c68581b89132ede8335b7799693b11f15590
Danielle Foss <danifoss2011@gmail.com>
2024-01-06 15:25:29 -0700

    add basic code and format to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,14 @@
 //import React from "react";
+import "./Footer.css";
 
-const Footer = () => {
-  console.log("hello from footer");
-
-  return console.log("hello from footer2");
-};
+function Footer() {
+  console.log("Fewter");
+  return (
+    <footer className="footer">
+      <div>Developed by Danielle Foss</div>
+      <div>2024</div>
+    </footer>
+  );
+}
 
 export default Footer;
