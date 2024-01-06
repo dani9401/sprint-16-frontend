abfdab04019a4242c2bb429edbd9c6f53c0b04aa
Danielle Foss <danifoss2011@gmail.com>
2024-01-06 15:18:26 -0700

    add switch, more imports, and routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,23 @@ import React from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
-//import Footer from "../Footer/Footer";
-import { Route } from "react-router-dom";
+import Profile from "../Profile/Profile";
+import Footer from "../Footer/Footer";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Woof Match</h1>
       <Header></Header>
-      <Route exact path="/">
-        <Main></Main>
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Main></Main>
+        </Route>
+        <Route path="/profile">
+          <Profile></Profile>
+        </Route>
+      </Switch>
+      <Footer></Footer>
     </div>
   );
 }
