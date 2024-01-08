53a01021e02ea998f1fed63cbc4eca25dcc6b9a1
Danielle Foss <danifoss2011@gmail.com>
2024-01-08 12:07:01 -0700

    add basic code for ModalWithForm

---
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -0,0 +1,34 @@
+import "../ModalWithForm/ModalWithForm.css";
+
+const ModalWithForm = ({
+  children,
+  title,
+  onClose,
+  modalName,
+  isOpen,
+  onSubmit,
+  buttonText,
+}) => {
+  //need to bring in Modal Names like react
+  //as prop
+  //if I want to style each modal differently
+
+  return (
+    <div className={`modal modal-type-${modalName}`}>
+      <div className={`modal__container modal-container-modalNameGoesHere`}>
+        <button
+          type="button"
+          onClick={onClose}
+          className="modal__close-button"
+        />
+        <h3 className="modal__title">{title}</h3>
+        <form className="modal__form" onSubmit={onSubmit}>
+          {children}
+          <button className="modal__submit-button">{buttonText}</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWithForm;
