e08f01cf72b52bc7f14bc3fb319813441b6a9b19
Danielle Foss <danifoss2011@gmail.com>
2024-01-08 15:27:57 -0700

    import react and make code pattern consistent with other componenets

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
-//import React from "react";
+import React from "react";
 import "./Footer.css";
 
-function Footer() {
+const Footer = () => {
   return (
     <footer className="footer">
       <div>Developed by Danielle Foss</div>
       <div>2024</div>
     </footer>
   );
-}
+};
 
 export default Footer;
