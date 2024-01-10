b0ef424260034e3b33513c2a974f233e78db7130
Danielle Foss <danifoss2011@gmail.com>
2024-01-10 12:41:41 -0700

    bring in item prop

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,29 +1,20 @@
 import React from "react";
 import "../Card/Card.css";
-import Card1 from "../../images/card1.png";
 
-const Card = ({ onSelectCard }) => {
+const Card = ({ onSelectCard, item }) => {
   return (
     <div className="card__element">
       <img
-        src={Card1}
+        src={item.imageUrl}
         className="card__image"
-        //onClick={() => onSelectCard()}
-        //alt={item.name}
+        onClick={() => onSelectCard()}
+        alt={item.name}
       />
       <div className="card__title">
-        <h2 className="card__name">Spot</h2>
-        <button
-          //className=""
-          type="button"
-          //onClick={() => handleCardLikeClick(item)}
-        ></button>
-        <h3>City, State</h3>
+        <h2 className="card__name">{item.name}</h2>
+        <h3>{item.breed}</h3>
       </div>
     </div>
-    // ------------ card 2
-
-    // -----------------------------------
   );
 };
 
