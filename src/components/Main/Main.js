887d465358d402b3c359a54113382d9cecd52acc
Danielle Foss <danifoss2011@gmail.com>
2024-01-10 12:40:21 -0700

    add dogList prop. pass to Card.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,18 @@
 import "../Main/Main.css";
 import Card from "../Card/Card";
 
-const Main = ({ onSelectCard }) => {
+const Main = ({ onSelectCard, dogList }) => {
   return (
     <main className="main">
       <h2>Mission Statement</h2>
       <section className="card__section" id="card">
         <h2 className="card__section-title">Title of card section, maybe</h2>
-        <Card onSelectCard={onSelectCard}></Card>
+        <div className="card__items" id="card-section">
+          {" "}
+          {dogList.map((item) => (
+            <Card onSelectCard={onSelectCard} item={item} key={item.id}></Card>
+          ))}
+        </div>
       </section>
     </main>
   );
